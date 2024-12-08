f863a74d67726e62c1bd90a130da99139fd42fcb
Nicolas Ramirez <nicolasramirez@agendapro.com>
2024-12-08 01:35:52 -0300

    first commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'SEO Title',
+ description: 'SEO Title',
+ keywords: ['About Page', 'SEO Title'],
+};
+
+export default function AboutPage() {
+    return (
+        <>
+            <span className="text-7xl">About Page</span>
+        </>
+    );
+}
---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'Contact Page',
+ description: 'Contact Page',
+};
+
+export default function ContactPage() {
+    return (
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    );
+}
